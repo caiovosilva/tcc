72b53ee0e93d4e1bd5d9a280a35dd8ad54ac7d21
Caio Silva <caiovosilva@outlook.com>
2020-01-13 13:57:28 -0300

    add xamarin project

---
+++ OuvindoErros/OuvindoErros/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using OuvindoErros.Services;
+using OuvindoErros.Views;
+
+namespace OuvindoErros
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
