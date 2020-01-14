f33c3733b9b92626ff99db6c65e030c7db423c20
Caio Silva <caiovosilva@outlook.com>
2020-01-14 17:19:03 -0300

    improve AudioRecorder plugin usage; asking for permissions

--- BipTranslator/BipTranslator/BipTranslator.Android/MainActivity.cs
+++ BipTranslator/BipTranslator/BipTranslator.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Content;
+using Android;
+using Android.Support.V4.App;
 
 namespace BipTranslator.Droid
 {
@@ -22,6 +25,10 @@ namespace BipTranslator.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.RecordAudio) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.RecordAudio }, 1);
+            }
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- BipTranslator/BipTranslator/BipTranslator/Views/ListeningPage.xaml.cs
+++ BipTranslator/BipTranslator/BipTranslator/Views/ListeningPage.xaml.cs
@@ -7,20 +7,104 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.ComponentModel;
 using Xamarin.Forms.Xaml;
+using Plugin.AudioRecorder;
 
 namespace BipTranslator.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListeningPage : ContentPage
     {
+        AudioRecorderService recorder;
+        AudioPlayer player;
         public ListeningPage()
         {
             InitializeComponent();
+
+            recorder = new AudioRecorderService
+            {
+                StopRecordingAfterTimeout = true,
+                TotalAudioTimeout = TimeSpan.FromSeconds(15),
+                AudioSilenceTimeout = TimeSpan.FromSeconds(2)
+            };
+
+            player = new AudioPlayer();
+            player.FinishedPlaying += Player_FinishedPlaying;
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+		async void Record_Clicked(object sender, EventArgs e)
+		{
+			await RecordAudio();
+		}
+
+		async Task RecordAudio()
+		{
+			try
+			{
+				if (!recorder.IsRecording) //Record button clicked
+				{
+					recorder.StopRecordingOnSilence = TimeoutSwitch.IsToggled;
+
+					RecordButton.IsEnabled = false;
+					PlayButton.IsEnabled = false;
+
+					//start recording audio
+					var audioRecordTask = await recorder.StartRecording();
+
+					RecordButton.Text = "Stop Recording";
+					RecordButton.IsEnabled = true;
+
+					await audioRecordTask;
+
+					RecordButton.Text = "Record";
+					PlayButton.IsEnabled = true;
+				}
+				else //Stop button clicked
+				{
+					RecordButton.IsEnabled = false;
+
+					//stop the recording...
+					await recorder.StopRecording();
+
+					RecordButton.IsEnabled = true;
+				}
+			}
+			catch (Exception ex)
+			{
+				//blow up the app!
+				throw ex;
+			}
+		}
+
+		void Play_Clicked(object sender, EventArgs e)
+		{
+			PlayAudio();
+		}
+
+		void PlayAudio()
+		{
+			try
+			{
+				var filePath = recorder.GetAudioFilePath();
+
+				if (filePath != null)
+				{
+					PlayButton.IsEnabled = false;
+					RecordButton.IsEnabled = false;
+
+					player.Play(filePath);
+				}
+			}
+			catch (Exception ex)
+			{
+				//blow up the app!
+				throw ex;
+			}
+		}
+
+		void Player_FinishedPlaying(object sender, EventArgs e)
         {
-            DisplayAlert("clicou hein!", "iniciando gravação agorinha", "OK");
+            PlayButton.IsEnabled = true;
+            RecordButton.IsEnabled = true;
         }
     }
 }
