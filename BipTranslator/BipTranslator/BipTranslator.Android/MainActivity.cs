a3b27ed4a2837f8377455f809ec950c99399b55e
Caio Silva <caiovosilva@outlook.com>
2020-01-15 20:34:04 -0300

    add UserDialogs and Permissions plugins

--- BipTranslator/BipTranslator/BipTranslator.Android/MainActivity.cs
+++ BipTranslator/BipTranslator/BipTranslator.Android/MainActivity.cs
@@ -9,14 +9,18 @@ using Android.OS;
 using Android.Support.V4.Content;
 using Android;
 using Android.Support.V4.App;
+using Plugin.Permissions;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
 
 namespace BipTranslator.Droid
 {
     [Activity(Label = "BipTranslator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected async override void OnCreate(Bundle savedInstanceState)
         {
+            UserDialogs.Init(this);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -25,29 +29,91 @@ namespace BipTranslator.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.RecordAudio) != Permission.Granted)
-            {
-                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.RecordAudio }, 1);
-            }
-            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted)
-            {
-                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.WriteExternalStorage }, 0);
-            }
-            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.ReadExternalStorage) != (int)Permission.Granted)
-            {
-                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.ReadExternalStorage }, 0);
-            }
-            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.ModifyAudioSettings) != (int)Permission.Granted)
-            {
-                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.ModifyAudioSettings }, 0);
-            }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            //Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+        async Task TryToGetPermissions()
+        {
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                await GetPermissionsAsync();
+                return;
+            }
+
+
+        }
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionsGroupLocation =
+            {
+                            //TODO add more permissions
+                            Manifest.Permission.AccessCoarseLocation,
+                            Manifest.Permission.AccessFineLocation,
+             };
+        async Task GetPermissionsAsync()
+        {
+            const string permission = Manifest.Permission.AccessFineLocation;
+
+            if (CheckSelfPermission(permission) == (int)Android.Content.PM.Permission.Granted)
+            {
+                //TODO change the message to show the permissions name
+                Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+                return;
+            }
+
+            if (ShouldShowRequestPermissionRationale(permission))
+            {
+                //set alert for executing the task
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Permissions Needed");
+                alert.SetMessage("The application need special permissions to continue");
+                alert.SetPositiveButton("Request Permissions", (senderAlert, args) =>
+                {
+                    RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+                });
+
+                alert.SetNegativeButton("Cancel", (senderAlert, args) =>
+                {
+                    Toast.MakeText(this, "Cancelled!", ToastLength.Short).Show();
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+
+
+                return;
+            }
+
+            RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+
+        }
+        //public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    switch (requestCode)
+        //    {
+        //        case RequestLocationId:
+        //            {
+        //                if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+        //                {
+        //                    Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+
+        //                }
+        //                else
+        //                {
+        //                    //Permission Denied :(
+        //                    Toast.MakeText(this, "Special permissions denied", ToastLength.Short).Show();
+
+        //                }
+        //            }
+        //            break;
+        //    }
+        //    //base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
     }
+    
 }
--- BipTranslator/BipTranslator/BipTranslator/ViewModels/ListeningViewModel.cs
+++ BipTranslator/BipTranslator/BipTranslator/ViewModels/ListeningViewModel.cs
@@ -1,14 +1,107 @@
-﻿using System;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Acr.UserDialogs;
+using System.Threading.Tasks;
 
 namespace BipTranslator.ViewModels
 {
     class ListeningViewModel : BaseViewModel
     {
-        public ListeningViewModel()
+        public ListeningViewModel(/*IUserDialogs dialogs*/)
         {
+            TryToGetPermissionsAsync();
+            //this.Dialogs = dialogs;
             Title = "Clica aê cabra!";
+        }
+
+        protected IUserDialogs Dialogs { get; }
+
+        private async Task TryToGetPermissionsAsync()
+        {
+            //try
+            //{
+            //    var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+            //    if (status != PermissionStatus.Granted)
+            //    {
+            //        if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+            //        {
+            //            await this.Dialogs.AlertAsync("need permissions");
+            //        }
+
+            //        var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Location });
+            //        status = results[Permission.Location];
+            //    }
+
+            //    if (status == PermissionStatus.Granted)
+            //    {
+            //        var results = await CrossGeolocator.Current.GetPositionAsync(10000);
+            //        LabelGeolocation.Text = "Lat: " + results.Latitude + " Long: " + results.Longitude;
+            //    }
+            //    else if (status != PermissionStatus.Unknown)
+            //    {
+            //        await DisplayAlert("Location Denied", "Can not continue, try again.", "OK");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    LabelGeolocation.Text = "Error: " + ex;
+            //}
+            bool permissionsGranted = true;
+
+            var permissionsStartList = new List<Permission>()
+        {
+            Permission.Location,
+            Permission.LocationAlways,
+            Permission.LocationWhenInUse,
+            Permission.Storage,
+            Permission.Microphone
+        };
+
+            var permissionsNeededList = new List<Permission>();
+            try
+            {
+                foreach (var permission in permissionsStartList)
+                {
+                    var status = await CrossPermissions.Current.CheckPermissionStatusAsync(permission);
+                    if (status != PermissionStatus.Granted)
+                    {
+                        permissionsNeededList.Add(permission);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            var results = await CrossPermissions.Current.RequestPermissionsAsync(permissionsNeededList.ToArray());
+
+            try
+            {
+                foreach (var permission in permissionsNeededList)
+                {
+                    var status = PermissionStatus.Unknown;
+                    //Best practice to always check that the key exists
+                    if (results.ContainsKey(permission))
+                        status = results[permission];
+                    if (status == PermissionStatus.Granted || status == PermissionStatus.Unknown)
+                    {
+                        permissionsGranted = true;
+                    }
+                    else
+                    {
+                        permissionsGranted = false;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            //return permissionsGranted;
         }
     }
 }
