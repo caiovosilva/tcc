96cf742981bc920d7f99793273b6c91b7944c2b2
Caio Silva <caiovosilva@outlook.com>
2020-02-03 13:44:03 -0300

    added class to convert audio file to binary

---
+++ BipTranslator/BipTranslator/BipTranslator/Services/Translator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BipTranslator.Services
+{
+    class Translator
+    {
+        public static BitArray GetBinaryFile(string fileName)
+        {
+            byte[] bytes = File.ReadAllBytes(fileName);
+            return new BitArray(bytes);
+        }
+    }
+}
